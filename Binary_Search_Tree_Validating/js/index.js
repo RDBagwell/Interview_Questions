a62e4778302a0b72bcaa076dd6a57bc1ef15e1a2
Robert Bagwell <erbagwell@yahoo.com>
2021-08-07 00:08:46 -0600

    add files

---
+++ Binary_Search_Tree_Validating/js/index.js
@@ -0,0 +1,50 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data){
+        if(data < this.data && this.left){
+            this.left.insert(data);
+        } else if (data < this.data){
+            this.left = new Node(data);
+        } else if (data > this.data && this.right){
+            this.right.insert(data);
+        } else if(data > this.data){
+            this.right = new Node(data);
+        }
+    }
+
+    contains(data){
+        if(data === this.data){
+            return this;
+        } 
+
+        if(this.data < data && this.right){
+           return this.right.contains(data);
+        } else if(this.data > data && this.left){
+            return this.left.contains(data);
+        }
+        return null;
+    }
+}
+
+function validate(node, min = null, max = null) {
+    if(max !== null && node.data > max) return false;
+
+    if(min !== null && node.data < min) return false;
+
+    if(node.left && !validate(node.left, min, node.data)) return false;
+
+    if(node.right && !validate(node.right, node.data, max)) return false;
+
+    return true;
+}
+
+const node = new Node(5)
+node.insert(10);
+node.insert(3);
+node.insert(11);
+console.log(validate(node));
+
